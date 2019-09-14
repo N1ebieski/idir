5e5aa2311ad6d56c073cf010b837bf5d5d9a2991
N1ebieski <mariusz.wysokinski@neostrada.pl>
2019-09-14 11:16:52 +0200

    first commit

---
+++ src/Providers/IDirServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace N1ebieski\IDir\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class IDirServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
