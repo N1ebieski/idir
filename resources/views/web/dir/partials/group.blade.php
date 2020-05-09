e7f5c42c4458e817b1f67af0e62c20224f048791
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-05-09 15:36:00 +0200

    icon for privileges in list group

--- resources/views/web/dir/partials/group.blade.php
+++ resources/views/web/dir/partials/group.blade.php
@@ -10,22 +10,35 @@
     <ul class="list-group list-group-flush">
         @if ($group->privileges->isNotEmpty())
         @foreach ($group->privileges as $privilege)
-        <li class="list-group-item">
-            {{ __($privilege->name) }}
+        <li class="list-group-item d-flex">
+            <i class="fas fa-check my-auto text-success"></i>
+            <div class="ml-3">{{ __($privilege->name) }}</div>
         </li>
         @endforeach
         @endif
-        <li class="list-group-item">
-            {{ mb_strtolower(trans('idir::groups.apply_status.label')) }}: {{ trans("idir::groups.apply_status.{$group->apply_status}") }}
+        <li class="list-group-item d-flex">
+            <i class="fas fa-check my-auto text-success"></i>
+            <div class="ml-3">
+                {{ mb_strtolower(trans('idir::groups.apply_status.label')) }}: {{ trans("idir::groups.apply_status.{$group->apply_status}") }}
+            </div>
         </li>
-        <li class="list-group-item">
-            {{ mb_strtolower(trans('idir::groups.max_cats.label')) }}: {{ $group->max_cats }}
+        <li class="list-group-item d-flex">
+            <i class="fas fa-check my-auto text-success"></i>
+            <div class="ml-3">
+                {{ mb_strtolower(trans('idir::groups.max_cats.label')) }}: {{ $group->max_cats }}
+            </div>
         </li>
-        <li class="list-group-item">
-            {{ mb_strtolower(trans('idir::groups.url.label')) }}: {{ trans("idir::groups.url.{$group->url}") }}
+        <li class="list-group-item d-flex">
+            <i class="fas fa-check my-auto text-success"></i>
+            <div class="ml-3">
+                {{ mb_strtolower(trans('idir::groups.url.label')) }}: {{ trans("idir::groups.url.{$group->url}") }}
+            </div>
         </li>
-        <li class="list-group-item">
-            {{ mb_strtolower(trans('idir::groups.backlink.label')) }}: {{ trans("idir::groups.backlink.{$group->backlink}") }}
+        <li class="list-group-item d-flex">
+            <i class="fas fa-check my-auto text-success"></i>
+            <div class="ml-3">
+                {{ mb_strtolower(trans('idir::groups.backlink.label')) }}: {{ trans("idir::groups.backlink.{$group->backlink}") }}
+            </div>
         </li>
         <li class="list-group-item">
             {{ mb_strtolower(trans('idir::groups.price')) }}:
