21df1a13d77cc6490fa78320d4008f14ddfb36ea
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-05-30 12:33:55 +0200

    fix

--- resources/views/web/profile/edit_dir.blade.php
+++ resources/views/web/profile/edit_dir.blade.php
@@ -1,19 +1,19 @@
 @extends(config('icore.layout') . '::web.profile.layouts.layout', [
-    'title' => [trans('idir::profile.page.edit_dir'), trans('icore::pagination.page', ['num' => $dirs->currentPage()])],
-    'desc' => [trans('idir::profile.page.edit_dir')],
-    'keys' => [trans('idir::profile.page.edit_dir')]
+    'title' => [trans('idir::profile.route.edit_dir'), trans('icore::pagination.page', ['num' => $dirs->currentPage()])],
+    'desc' => [trans('idir::profile.route.edit_dir')],
+    'keys' => [trans('idir::profile.route.edit_dir')]
 ])
 
 @section('breadcrumb')
 <li class="breadcrumb-item"><a href="/">{{ trans('icore::home.route.index') }}</a></li>
 <li class="breadcrumb-item">{{ trans('icore::profile.route.index') }}</li>
-<li class="breadcrumb-item active" aria-current="page">{{ trans('idir::profile.page.edit_dir') }}</li>
+<li class="breadcrumb-item active" aria-current="page">{{ trans('idir::profile.route.edit_dir') }}</li>
 @endsection
 
 @section('content')
 <h1 class="h4 border-bottom pb-2">
     <i class="far fa-fw fa-folder-open"></i>
-    <span>{{ trans('idir::profile.page.edit_dir') }}</span>
+    <span>{{ trans('idir::profile.route.edit_dir') }}</span>
 </h1>
 <div id="filterContent">
     @include('idir::web.profile.partials.dir.filter')
