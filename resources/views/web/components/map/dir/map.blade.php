8957f88ae0d2e9ced42e570319a1cb8d0f64b16e
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-08-04 19:13:35 +0200

    fix async google map asset

--- resources/views/web/components/map/dir/map.blade.php
+++ resources/views/web/components/map/dir/map.blade.php
@@ -3,6 +3,6 @@ data-address-marker="{{ $addressMarker ?? null }}" data-zoom="{{ $zoom }}"
 data-coords-marker="{{ $coordsMarker ?? null }}"></div>
 
 @pushonce('script.map')
-<script async defer src="https://maps.googleapis.com/maps/api/js?key={{ config('services.googlemap.api_key') }}&callback=initMap" 
+<script defer src="https://maps.googleapis.com/maps/api/js?key={{ config('services.googlemap.api_key') }}&callback=initMap" 
 type="text/javascript"></script>
 @endpushonce
