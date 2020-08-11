5079e0ac27a111acfb9f38b06f9d58876b0315f8
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-08-11 13:26:30 +0200

    add polymorphic morphs relation

--- src/Models/Tag/Dir/Tag.php
+++ src/Models/Tag/Dir/Tag.php
@@ -2,10 +2,24 @@
 
 namespace N1ebieski\IDir\Models\Tag\Dir;
 
+use Illuminate\Support\Facades\Config;
 use N1ebieski\ICore\Models\Tag\Tag as BaseTagModel;
 
 class Tag extends BaseTagModel
 {
+    // Relations
+
+    /**
+     * [morphs description]
+     * @return [type] [description]
+     */
+    public function morphs()
+    {
+        $table = Config::get('taggable.tables.taggable_taggables', 'taggable_taggables');
+
+        return $this->morphedByMany(\N1ebieski\IDir\Models\Dir::class, 'taggable', $table, 'tag_id');
+    }
+
     // Accessors
 
     /**
