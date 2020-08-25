de15b4049056321e3d9f057d297ca867dc5aac19
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-08-25 23:34:52 +0200

    default group with nofollow link privilege

--- database/seeds/Install/DefaultGroupAndPrivilegesSeeder.php
+++ database/seeds/Install/DefaultGroupAndPrivilegesSeeder.php
@@ -19,19 +19,19 @@ class DefaultGroupAndPrivilegesSeeder extends Seeder
     public function run()
     {
         // create privileges
-        Privilege::create(['name' => 'highest position on homepage']);
-        Privilege::create(['name' => 'highest position in their categories']);
-        Privilege::create(['name' => 'highest position in ancestor categories']);
-        Privilege::create(['name' => 'highest position in search results']);
-        Privilege::create(['name' => 'additional link on the friends subpage']);
-        Privilege::create(['name' => 'direct link nofollow']);
-        Privilege::create(['name' => 'direct link on listings']);
-        Privilege::create(['name' => 'place in the links component']);
-        Privilege::create(['name' => 'place in the advertising component']);
-        Privilege::create(['name' => 'additional options for editing content']);
+        Privilege::firstOrCreate(['name' => 'highest position on homepage']);
+        Privilege::firstOrCreate(['name' => 'highest position in their categories']);
+        Privilege::firstOrCreate(['name' => 'highest position in ancestor categories']);
+        Privilege::firstOrCreate(['name' => 'highest position in search results']);
+        Privilege::firstOrCreate(['name' => 'additional link on the friends subpage']);
+        Privilege::firstOrCreate(['name' => 'direct link on listings']);
+        Privilege::firstOrCreate(['name' => 'place in the links component']);
+        Privilege::firstOrCreate(['name' => 'place in the advertising component']);
+        Privilege::firstOrCreate(['name' => 'additional options for editing content']);
 
-        $default = Group::create([
-            'id' => 1,
+        $nofollow = Privilege::firstOrCreate(['name' => 'direct link nofollow']);
+
+        $default = Group::firstOrCreate(['id' => 1], [
             'name' => 'Default',
             'max_cats' => 3,
             'position' => 0,
@@ -40,5 +40,7 @@ class DefaultGroupAndPrivilegesSeeder extends Seeder
             'apply_status' => Group::APPLY_INACTIVE,
             'url' => Group::OPTIONAL_URL
         ]);
+
+        $default->privileges()->sync([$nofollow->id]);
     }
 }
