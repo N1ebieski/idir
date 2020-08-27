89feedd1e564baa78a24945d5341d4a1df14a758
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-08-27 13:05:57 +0200

    refactor

--- resources/views/web/components/map/dir/map.blade.php
+++ resources/views/web/components/map/dir/map.blade.php
@@ -1,8 +1,15 @@
-<div id="{{ $selector }}" data-coords="{{ $coords }}" data-container-class="{{ $containerClass }}"
-data-address-marker="{{ $addressMarker ?? null }}" data-zoom="{{ $zoom }}"
-data-coords-marker="{{ $coordsMarker ?? null }}"></div>
+<div 
+    id="{{ $selector }}"
+    data-coords="{{ $coords }}" 
+    data-container-class="{{ $containerClass }}"
+    data-address-marker="{{ $addressMarker ?? null }}" 
+    data-zoom="{{ $zoom }}"
+    data-coords-marker="{{ $coordsMarker ?? null }}"
+></div>
 
 @pushonce('script.map')
-<script defer src="https://maps.googleapis.com/maps/api/js?key={{ config('services.googlemap.api_key') }}&callback=initMap" 
-type="text/javascript"></script>
+<script defer 
+    src="https://maps.googleapis.com/maps/api/js?key={{ config('services.googlemap.api_key') }}&callback=initMap" 
+    type="text/javascript"
+></script>
 @endpushonce
