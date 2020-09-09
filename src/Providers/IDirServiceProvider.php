aa6b1824d339dcca49a3db9a85ef8e0ea5a8ec6d
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-09-09 14:15:50 +0200

    version

--- src/Providers/IDirServiceProvider.php
+++ src/Providers/IDirServiceProvider.php
@@ -13,7 +13,7 @@ class IDirServiceProvider extends ServiceProvider
      * [public description]
      * @var string
      */
-    public const VERSION = "1.0.0-rc.20";
+    public const VERSION = "1.0.0-rc.21";
 
     /**
      * Register services.
