13a090f70613e6c803601a2d562839cbfb59a2aa
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-11-16 12:07:08 +0100

    en lang

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -4,5 +4,5 @@ return [
     'code' => 'The code is incorrect.',
     'backlink_url' => 'If there is a url, the backlink must also be on this domain.',
     'backlink' => 'No backlink was found at this address.',
-    'email' => 'This email address already exists in the database. <a href=":login">Log in</a> to add an entry to your account.'
+    'email' => 'This email address already exists in the database. <a href=":login">Log in</a> to your account or <a href=":reset">use the password reset option</a> to access your account.'
 ];
