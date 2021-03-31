f9dee86a49f0ff66358258fbda65c456c2e1d842
N1ebieski <mariusz@wysokinski.org>
2021-03-31 14:16:26 +0200

    Update IDirServiceProvider.php

--- src/Providers/IDirServiceProvider.php
+++ src/Providers/IDirServiceProvider.php
@@ -13,7 +13,7 @@ class IDirServiceProvider extends ServiceProvider
      * [public description]
      * @var string
      */
-    public const VERSION = "3.2.8";
+    public const VERSION = "3.2.9";
 
     /**
      * Register services.
