c8940e96108db70c3677f55f4077fed42df064ce
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-04-20 13:29:07 +0200

    fix

--- database/migrations/2021_04_20_111030_add_column_fulltext_index_to_dirs_table.php
+++ database/migrations/2021_04_20_111030_add_column_fulltext_index_to_dirs_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnFulltextIndexToAllTables extends Migration
+class AddColumnFulltextIndexToDirsTable extends Migration
 {
     /**
      * Run the migrations.
