e112d655b5028290d39e2752c09947328aac024b
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-08-21 01:06:47 +0200

    fix

--- src/Http/Requests/Admin/Role/UpdateRequest.php
+++ src/Http/Requests/Admin/Role/UpdateRequest.php
@@ -9,13 +9,5 @@ use N1ebieski\ICore\Http\Requests\Admin\Role\UpdateRequest as BaseUpdateRequest;
  */
 class UpdateRequest extends BaseUpdateRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return parent::rules()
-    }
+    //
 }
