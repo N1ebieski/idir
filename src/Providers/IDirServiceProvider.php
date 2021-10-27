10822d6bcc52ae85b690da109fd4f4577cce4f5f
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-10-27 12:50:00 +0200

    custom file input multiple

--- src/Providers/IDirServiceProvider.php
+++ src/Providers/IDirServiceProvider.php
@@ -10,7 +10,7 @@ class IDirServiceProvider extends ServiceProvider
      * [public description]
      * @var string
      */
-    public const VERSION = "5.1.4";
+    public const VERSION = "5.1.5";
 
     /**
      * Register services.
