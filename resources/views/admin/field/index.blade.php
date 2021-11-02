b1d09262064337e8ec86ed1f572725971091548e
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-11-02 11:00:37 +0100

    fix

--- resources/views/admin/field/index.blade.php
+++ resources/views/admin/field/index.blade.php
@@ -10,11 +10,6 @@
 ])
 
 @section('breadcrumb')
-<li class="breadcrumb-item">
-    <a href="{{ route('admin.home.index') }}">
-        {{ trans('icore::home.route.index') }}
-    </a>
-</li>
 <li class="breadcrumb-item active" aria-current="page">
     {{ trans('idir::fields.route.index') }}
 </li>
