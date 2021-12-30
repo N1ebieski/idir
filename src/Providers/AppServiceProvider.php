cff40a1829909c36410caf7f1c5153a82060dc6d
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-12-30 13:49:43 +0100

    change ioc binding method

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -13,22 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(\N1ebieski\ICore\Utils\FileUtil::class, function ($app) {
-            return $app->make(\N1ebieski\IDir\Utils\FileUtil::class, [
-                'path' => ''
-            ]);
-        });
+        $this->app->bind(\N1ebieski\ICore\Utils\FileUtil::class, \N1ebieski\IDir\Utils\FileUtil::class);
 
-        $this->app->bind(\N1ebieski\IDir\Utils\ThumbnailUtil::class, function ($app, $with) {
-            return new \N1ebieski\IDir\Utils\ThumbnailUtil(
-                $app->make(\N1ebieski\IDir\Http\Clients\Thumbnail\Provider\Client::class),
-                $app->make(\Illuminate\Contracts\Filesystem\Factory::class),
-                $app->make(\Illuminate\Support\Carbon::class),
-                $app->make(\Illuminate\Contracts\Config\Repository::class),
-                $with['url'] ?? '',
-                $with['disk'] ?? 'public'
-            );
-        });
+        $this->app->when(\N1ebieski\IDir\Utils\ThumbnailUtil::class)
+            ->needs('$url')
+            ->give('');
     }
 
     /**
