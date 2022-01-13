b43f8a2617fe798ac9b41637f5c18b74221d4d3a
N1ebieski <mariusz.wysokinski@neostrada.pl>
2022-01-13 15:08:07 +0100

    option to disable reminder and change run cron

--- src/Providers/ScheduleServiceProvider.php
+++ src/Providers/ScheduleServiceProvider.php
@@ -2,11 +2,19 @@
 
 namespace N1ebieski\IDir\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Console\Scheduling\Schedule;
 
 class ScheduleServiceProvider extends ServiceProvider
 {
+    /**
+     * Undocumented variable
+     *
+     * @var Schedule
+     */
+    protected $schedule;
+
     /**
      * Register services.
      *
@@ -25,38 +33,53 @@ class ScheduleServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->booted(function () {
-            $schedule = $this->app->make(Schedule::class);
+            $this->schedule = $this->app->make(Schedule::class);
 
-            $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\BacklinkCron::class))
+            $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\BacklinkCron::class))
                 ->name('BacklinkCron')
                 ->daily()
                 ->runInBackground();
 
-            $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\CompletedCron::class))
+            $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\CompletedCron::class))
                 ->name('CompletedCron')
                 ->daily()
                 ->runInBackground();
 
-            // TODO #85 launch date set by IDIR_DIR_REMINDER_LEFT_DAYS as prepareClearCacheSchedule
-            $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\ReminderCron::class))
-                ->name('ReminderCron')
-                ->weekly()
-                ->runInBackground();
+            $this->callReminderSchedule();
 
-            $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\StatusCron::class))
+            $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\StatusCron::class))
                 ->name('StatusCron')
                 ->daily()
                 ->runInBackground();
 
-            $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\ModeratorNotificationCron::class))
+            $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\ModeratorNotificationCron::class))
                 ->name('ModeratorNotificationCron')
                 ->hourly()
                 ->runInBackground();
 
-            $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Sitemap\SitemapCron::class))
+            $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Sitemap\SitemapCron::class))
                 ->name('SitemapCron')
                 ->daily()
                 ->runInBackground();
         });
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    protected function callReminderSchedule(): void
+    {
+        $days = Config::get('idir.dir.reminder.left_days');
+
+        if ($days <= 0 || $days > 30) {
+            return;
+        }
+
+        $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\ReminderCron::class))
+            ->name('ReminderCron')
+            ->cron("0 0 */{$days} * *")
+            ->runInBackground();
     }
 }
