0de68f9336fd164836fb34d958c9a4f93b009a2e
N1ebieski <mariusz.wysokinski@neostrada.pl>
2022-02-08 23:06:57 +0100

    add profile tokens route

--- resources/views/web/profile/partials/sidebar.blade.php
+++ resources/views/web/profile/partials/sidebar.blade.php
@@ -16,23 +16,37 @@
     <li class="nav-item {{ $isUrl(route('web.profile.socialites')) }}">
         <a 
             class="nav-link {{ $isUrl(route('web.profile.socialites')) }}"
-            title="{{ trans('icore::profile.route.edit_socialite') }}"
+            title="{{ trans('icore::profile.route.socialites') }}"
             href="{{ route('web.profile.socialites') }}"
         >
             <i class="fab fa-fw fa-facebook-square"></i>
-            <span>{{ trans('icore::profile.route.edit_socialite') }}</span>
+            <span>{{ trans('icore::profile.route.socialites') }}</span>
         </a>
     </li>
     @endif
+    @canany(['web.tokens.edit', 'web.tokens.delete'])
+    @can('api.access')
+    <li class="nav-item {{ $isUrl(route('web.profile.tokens')) }}">
+        <a 
+            class="nav-link {{ $isUrl(route('web.profile.tokens')) }}"
+            title="{{ trans('icore::profile.route.tokens') }}"
+            href="{{ route('web.profile.tokens') }}"
+        >
+            <i class="fas fa-fw fa-user-lock"></i>
+            <span>{{ trans('icore::profile.route.tokens') }}</span>
+        </a>
+    </li>
+    @endcan
+    @endcan    
     @canany(['web.dirs.edit', 'web.dirs.delete'])
     <li class="nav-item {{ $isUrl(route('web.profile.dirs')) }}">
         <a 
             class="nav-link {{ $isUrl(route('web.profile.dirs')) }}"
-            title="{{ trans('idir::profile.route.edit_dir') }}"
+            title="{{ trans('idir::profile.route.dirs') }}"
             href="{{ route('web.profile.dirs') }}"
         >
             <i class="far fa-fw fa-folder-open"></i>
-            <span>{{ trans('idir::profile.route.edit_dir') }}</span>
+            <span>{{ trans('idir::profile.route.dirs') }}</span>
         </a>
     </li>
     @endcan
--- routes/web/profile.php
+++ routes/web/profile.php
@@ -5,5 +5,6 @@ use N1ebieski\IDir\Http\Controllers\Web\ProfileController;
 
 Route::group(['middleware' => 'auth'], function () {
     Route::match(['post', 'get'], 'profile/dirs', [ProfileController::class, 'dirs'])
-        ->name('profile.dirs');
+        ->name('profile.dirs')
+        ->middleware(['api.access', 'permission:web.dirs.edit|web.dirs.delete']);
 });
