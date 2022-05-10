b07e8935d3035e6a613cd2c2a285074c5cda6b32
N1ebieski <mariusz.wysokinski@neostrada.pl>
2022-05-10 14:32:25 +0200

    move binds to license

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -13,62 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(\N1ebieski\ICore\Models\Token\PersonalAccessToken::class, \N1ebieski\IDir\Models\Token\PersonalAccessToken::class);
-
-        $this->app->when(\N1ebieski\ICore\Utils\File\File::class)
-            ->needs('$temp_path')
-            ->give('vendor/idir/temp');
-
-        $this->app->when(\N1ebieski\IDir\Utils\Thumbnail\Thumbnail::class)
-            ->needs('$url')
-            ->give('');
-
-        $this->app->when(\N1ebieski\IDir\Console\Commands\Update\UpdateCommand::class)
-            ->needs('$backup_path')
-            ->give('backup/vendor/idir');
-
-        $this->app->when(\N1ebieski\IDir\Console\Commands\Update\RollbackCommand::class)
-            ->needs('$backup_path')
-            ->give('backup/vendor/idir');
-
-        $this->app->when(\N1ebieski\IDir\Console\Commands\Update\UpdateCommand::class)
-            ->needs(\N1ebieski\ICore\Console\Commands\Update\SchemaFactory::class)
-            ->give(function () {
-                return $this->app->make(\N1ebieski\IDir\Console\Commands\Update\SchemaFactory::class);
-            });
-
-        $this->app->bind(\N1ebieski\IDir\Http\Clients\Payment\Interfaces\Codes\SmsClientInterface::class, function ($app) {
-            switch ($app['config']['idir.payment.code_sms.driver']) {
-                case 'cashbill':
-                    return $app->make(\N1ebieski\IDir\Http\Clients\Payment\Cashbill\Codes\SMS\SmsClient::class);
-            }
-        });
-
-        $this->app->bind(\N1ebieski\IDir\Http\Clients\Payment\Interfaces\Codes\TransferClientInterface::class, function ($app) {
-            switch ($app['config']['idir.payment.code_transfer.driver']) {
-                case 'cashbill':
-                    return $app->make(\N1ebieski\IDir\Http\Clients\Payment\Cashbill\Codes\Transfer\TransferClient::class);
-            }
-        });
-
-        $this->app->bind(\N1ebieski\IDir\Http\Clients\Payment\Interfaces\Transfer\TransferClientInterface::class, function ($app) {
-            $driver = $app['request']->route('driver') ?? $app['config']['idir.payment.transfer.driver'];
-
-            return $app->make(\N1ebieski\IDir\Http\Clients\Payment\Factories\TransferClientFactory::class)->makeClient($driver);
-        });
-
-        $this->app->bind(\N1ebieski\IDir\Http\Requests\Web\Payment\Interfaces\CompleteRequestInterface::class, function ($app) {
-            $driver = $app['request']->route('driver') ?? $app['config']['idir.payment.transfer.driver'];
-
-            return (new \N1ebieski\IDir\Http\Requests\Web\Payment\Factories\CompleteRequestFactory())->makeRequest($driver);
-        });
-
-
-        $this->app->bind(\N1ebieski\IDir\Http\Requests\Api\Payment\Interfaces\VerifyRequestInterface::class, function ($app) {
-            $driver = $app['request']->route('driver') ?? $app['config']['idir.payment.transfer.driver'];
-
-            return (new \N1ebieski\IDir\Http\Requests\Api\Payment\Factories\VerifyRequestFactory())->makeRequest($driver);
-        });
+        //
     }
 
     /**
@@ -78,20 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Illuminate\Database\Eloquent\Relations\Relation::morphMap([
-            \N1ebieski\ICore\Models\User::class => \N1ebieski\IDir\Models\User::class
-        ]);
-
-        $aliasLoader = \Illuminate\Foundation\AliasLoader::getInstance();
-
-        foreach (glob(__DIR__ . '/../ValueObjects/**/*.php') as $classPath) {
-            if (!preg_match('/ValueObjects\/([A-Za-z\/]+).php/', $classPath, $matches)) {
-                continue;
-            }
-
-            $alias = str_replace('/', '\\', $matches[1]);
-
-            $aliasLoader->alias($alias, 'N1ebieski\\IDir\\ValueObjects\\' . $alias);
-        }
+        //
     }
 }
