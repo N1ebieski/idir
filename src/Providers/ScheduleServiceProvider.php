26fabf5e202a0a6c6b2f61e7c7ce0a99935ec687
N1ebieski <kontakt@intelekt.net.pl>
2022-10-20 22:32:34 +0000

    fix schedule

--- src/Providers/ScheduleServiceProvider.php
+++ src/Providers/ScheduleServiceProvider.php
@@ -21,16 +21,10 @@ namespace N1ebieski\IDir\Providers;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Container\BindingResolutionException;
 
 class ScheduleServiceProvider extends ServiceProvider
 {
-    /**
-     * Undocumented variable
-     *
-     * @var Schedule
-     */
-    protected $schedule;
-
     /**
      * Bootstrap services.
      *
@@ -38,39 +32,42 @@ class ScheduleServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->schedule = $this->app->make(Schedule::class);
+        if ($this->app->runningInConsole()) {
+            $this->app->booted(function () {
+                $schedule = $this->app->make(Schedule::class);
 
-        $this->app->booted(function () {
-            $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\BacklinkCron::class))
-                ->name('BacklinkCron')
-                ->daily();
+                $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\BacklinkCron::class))
+                    ->name('BacklinkCron')
+                    ->daily();
 
-            $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\CompletedCron::class))
-                ->name('CompletedCron')
-                ->daily();
+                $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\CompletedCron::class))
+                    ->name('CompletedCron')
+                    ->daily();
 
-            $this->callReminderSchedule();
+                $this->callReminderSchedule($schedule);
 
-            $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\StatusCron::class))
-                ->name('StatusCron')
-                ->daily();
+                $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\StatusCron::class))
+                    ->name('StatusCron')
+                    ->daily();
 
-            $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\ModeratorNotificationCron::class))
-                ->name('ModeratorNotificationCron')
-                ->hourly();
+                $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\ModeratorNotificationCron::class))
+                    ->name('ModeratorNotificationCron')
+                    ->hourly();
 
-            $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Sitemap\SitemapCron::class))
-                ->name('SitemapCron')
-                ->daily();
-        });
+                $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Sitemap\SitemapCron::class))
+                    ->name('SitemapCron')
+                    ->daily();
+            });
+        }
     }
 
     /**
-     * Undocumented function
      *
+     * @param Schedule $schedule
      * @return void
+     * @throws BindingResolutionException
      */
-    protected function callReminderSchedule(): void
+    protected function callReminderSchedule(Schedule $schedule): void
     {
         $days = Config::get('idir.dir.reminder.left_days');
 
@@ -78,7 +75,7 @@ class ScheduleServiceProvider extends ServiceProvider
             return;
         }
 
-        $this->schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\ReminderCron::class))
+        $schedule->call($this->app->make(\N1ebieski\IDir\Crons\Dir\ReminderCron::class))
             ->name('ReminderCron')
             ->cron("5 0 */{$days} * *");
     }
