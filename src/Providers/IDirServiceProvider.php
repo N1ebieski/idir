edfa9a03f44d67f05ebb48b54e13061c2454abca
N1ebieski <kontakt@intelekt.net.pl>
2024-06-14 10:29:28 +0000

    version

--- src/Providers/IDirServiceProvider.php
+++ src/Providers/IDirServiceProvider.php
@@ -27,7 +27,7 @@ class IDirServiceProvider extends ServiceProvider
      * [public description]
      * @var string
      */
-    public const VERSION = "10.3.5";
+    public const VERSION = "10.4.0";
 
     /**
      * Register services.
